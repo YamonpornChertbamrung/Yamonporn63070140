8edfefc30e44f12c2da37d8acea8a71982dfa5c8
YamonpornChertbamrung <73680098+YamonpornChertbamrung@users.noreply.github.com>
2020-10-30 13:02:28 +0700

    Create story.php

---
+++ story.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>HELLO a Little rabbit</title>
+    </head>
+    <body>
+        <h1>name surname nickname</h1>
+        <p>Yamonporn Chertbamrung Modji</p>
+        <img src="rabbit.jpg">
+        <img src="rabbits.jpg">
+        <a href="https://www.facebook.com">This is a Link</a>
+
+        <h2>Rabbits STORY</h2>
+        <p>ณ กลางป่าใหญ่
+
+            กระต่าย1 : นายๆ รู้มั้ยอะไรใหญ่กว่าปลาวาฬ
+            
+            กระต่าย2 : ก็ไม่รู้สินะ อะไรเหรอ
+            
+            กระต่าย1 : ปลาวาฬชุบแป้งทอดไง555555
+            
+            กระต่าย2 : เอิ่ม....
+            
+        </p>
+    </body>
+</html>
