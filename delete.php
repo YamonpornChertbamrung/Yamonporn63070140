dde5ddd85673253891e4af1485a10e063e9187bc
YamonpornChertbamrung <73680098+YamonpornChertbamrung@users.noreply.github.com>
2020-11-20 20:42:40 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -11,7 +11,12 @@ $sql = 'DELETE FROM guestbook WHERE ID = ' . $_GET['ID'] . '';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Delete</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-</head>
+    <style>
+    body{
+        background-image: url('code1.jpg');
+    }
+    </style>
+    </head>
 
 <body>
     <div class="container mt-5 text-center">
