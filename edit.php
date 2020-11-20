cdd5d13e7dbc5fb87948901a7cb52f6c30084ccc
YamonpornChertbamrung <73680098+YamonpornChertbamrung@users.noreply.github.com>
2020-11-20 20:26:24 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -41,7 +41,7 @@
                   <textarea rows="5" class="form-control" cols="20" name="comment" id="idComment" ><?php echo $data['Comment'];?></textarea><br>
                   <label class="m-3" for="link">Link</label>
                   <input type="text" class="form-control" name="link" id="idLink" value="<?php echo $data['Link'];?>">
-                  <input class="btn btn-primary mt-5" type="submit" id="commentBtn">
+                  <input class="btn btn-dark mt-5" type="submit" id="commentBtn">
               </div>
           </form>
       </div>
