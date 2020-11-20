77d12652ef20461e9146c38e058e68259c78da84
YamonpornChertbamrung <73680098+YamonpornChertbamrung@users.noreply.github.com>
2020-11-20 16:42:34 +0700

    Update show.php

--- show.php
+++ show.php
@@ -43,7 +43,7 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
     <td>
-      <a class="btn btn-success" href="edit.php?ID=<?php echo $Result['ID']; ?>">EDIT</a>
+      <a class="btn btn-warning" href="edit.php?ID=<?php echo $Result['ID']; ?>">EDIT</a>
       <a class="btn btn-info" href="delete.php?ID=<?php echo $Result['ID']; ?>">DELETE</a>
     </td>
   </tr>
