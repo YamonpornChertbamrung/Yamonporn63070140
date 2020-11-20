17e7fbfb6fd03dbfdf1ef49d595e676a8cc3c925
YamonpornChertbamrung <73680098+YamonpornChertbamrung@users.noreply.github.com>
2020-11-20 15:13:15 +0700

    Update itf.php

--- itf.php
+++ itf.php
@@ -4,24 +4,27 @@
   <title>DATABASE</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 
 <body>
   <?php
   $conn = mysqli_init();
-  mysqli_real_connect($conn, 'itflabs02.mysql.database.azure.com', 'yamonporn@itflabs02', 'Modji2002', 'ITFlab', 3306);
+  mysqli_real_connect($conn, 'apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab', 3306);
   if (mysqli_connect_errno($conn)) {
     die('Failed to connect to MySQL: ' . mysqli_connect_error());
   }
   $res = mysqli_query($conn, 'SELECT * FROM guestboook');
   ?>
-  <div class="container">
-  <table class="table table-dark table-striped">
-          <thead>
+  <div class="container mt-5">
+    <div class="card-header bg-primary text-white d-flex justify-content-between">
+      <h3>HOME</h4>
+       <a href="form.php" class="btn btn-success">ADD</a>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table table-hover table-bordered table-sm">
+          <thead class="thead-dark">
             <tr>
               <th width="300">
                 <div align="center">Name</div>
