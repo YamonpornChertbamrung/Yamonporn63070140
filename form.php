663abe8bb2b3470bab394ef094be87763ee7f26d
YamonpornChertbamrung <73680098+YamonpornChertbamrung@users.noreply.github.com>
2020-11-20 21:06:00 +0700

    Update form.php

--- form.php
+++ form.php
@@ -6,10 +6,15 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <style>
+    body{
+        background-image: url('img_girl.jpg');
+    }
+  </style>
 </head>
 <body>
   <div class="container">
-    <div class="card-header bg-primary text-white d-flex justify-content-between">
+     <div class="container p-3 my-3 bg-dark text-white">
      <h3>ADD</h4>
      <a href="itf.php" class="btn btn-light">BACK</a>
     </div>
@@ -22,7 +27,7 @@
           placeholder="Enter Comment"></textarea><br>
         <label class="m-3" for="link">Link</label>
         <input type="text" class="form-control" name="link" id="idLink" placeholder="Enter Link">
-        <input class="btn btn-success mt-5" type="submit" id="commentBtn">
+        <input class="btn btn-dark mt-5" type="submit" id="commentBtn">
       </div>
     </form>
   </div>
