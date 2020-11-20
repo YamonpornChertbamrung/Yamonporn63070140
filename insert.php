6ea361be5d792f3bb13507f02fe18a56e0e003ed
YamonpornChertbamrung <73680098+YamonpornChertbamrung@users.noreply.github.com>
2020-11-20 15:05:09 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -16,11 +16,31 @@ $link = $_POST['link'];
 $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
 
 
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
-mysqli_close($conn);
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Insert</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container mt-5 text-center">
+      <h3>
+        <?php
+        if (mysqli_query($conn, $sql)) {
+          echo "New record created successfully";
+        } else {
+          echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+        mysqli_close($conn);
+        ?>
+      </h3>
+      <a href="itf" class="btn btn-light mt-3">BACK</a>
+    </div>
+  </div>
+</body>
+
+</html>
