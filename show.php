f5c9d90f3a2f1130a2c35445f49a4df7d4135f34
YamonpornChertbamrung <73680098+YamonpornChertbamrung@users.noreply.github.com>
2020-11-20 17:09:34 +0700

    Update show.php

--- show.php
+++ show.php
@@ -9,51 +9,53 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'itflabs02.mysql.database.azure.com', 'yamonporn@itflabs02', 'Modji2002', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
-?>
- <div class="container">
-  <table class="table table-dark">
-          <thead>
-            <tr>
-              <th width="300">
-                <div align="center">Name</div>
-              </th>
-              <th width="300">
-                <div align="center">Comment </div>
-              </th>
-              <th width="300">
-                <div align="center">Action</div>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-    <td>
-      <a class="btn btn-info" href="delete.php?ID=<?php echo $Result['ID']; ?>">ลบ</a>
-      <a class="btn btn-warning" href="edit.php?ID=<?php echo $Result['ID']; ?>">แก้ไข</a>
-    </td>
-  </tr>
-<?php
-}
-?>
-</table>
-<a href="form.php" class="btn btn-success">ADD</a>
-<?php
-mysqli_close($conn);
-?>
+  <center>
+  <?php
+  $conn = mysqli_init();
+  mysqli_real_connect($conn, 'itflabs02.mysql.database.azure.com', 'yamonporn@itflabs02', 'Modji2002', 'itflab', 3306);
+  if (mysqli_connect_errno($conn))
+  {
+      die('Failed to connect to MySQL: '.mysqli_connect_error());
+  }
+  $res = mysqli_query($conn, 'SELECT * FROM guestbook');
+  ?>
+   <div class="container">
+    <table class="table table-dark">
+            <thead>
+              <tr>
+                <th width="300">
+                  <div align="center">Name</div>
+                </th>
+                <th width="300">
+                  <div align="center">Comment </div>
+                </th>
+                <th width="300">
+                  <div align="center">Action</div>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+  <?php
+  while($Result = mysqli_fetch_array($res))
+  {
+  ?>
+    <tr>
+      <td><?php echo $Result['Name'];?></div></td>
+      <td><?php echo $Result['Comment'];?></td>
+      <td><?php echo $Result['Link'];?></td>
+      <td>
+        <a class="btn btn-info" href="delete.php?ID=<?php echo $Result['ID']; ?>">ลบ</a>
+        <a class="btn btn-warning" href="edit.php?ID=<?php echo $Result['ID']; ?>">แก้ไข</a>
+      </td>
+    </tr>
+  <?php
+  }
+  ?>
+  </table>
+  <a href="form.php" class="btn btn-success">ADD</a>
+  <?php
+  mysqli_close($conn);
+  ?>
+  </center>
 </body>
 </html>
